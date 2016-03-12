9bf67f552a4ab0f6153da04187a969abd99f487b
getspeed <danilo89ds@gmail.com>
2016-03-12 15:25:58 +0100

    List funktioniert nun

---
+++ getUsers.js
@@ -0,0 +1,12 @@
+/**
+ * Created by getspeed on 12.03.16.
+ */
+
+$.getJSON( "ajax/users.php", function( data ) {
+    $.each( data, function( key, val ) {
+        $("tbody").append("<tr><td>" + val + "</td></tr>");
+    });
+});
+
+
+
