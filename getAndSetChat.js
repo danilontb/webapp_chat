214fefdc9ca6f43f9f27b1295369d892f3957102
getspeed <danilo89ds@gmail.com>
2016-03-13 22:48:57 +0100

    jetzt könnte es funktionieren. Mit vorbehalt

--- getAndSetChat.js
+++ getAndSetChat.js
@@ -2,9 +2,10 @@
  * Created by getspeed on 13.03.16.
  */
 $(document).ready(function() {
-    $('#btnsend').click(function () {
-        var text = document.getElementById("sendText").value;
+    $('#btnsend').click(function() {
 
+        var text = document.getElementById("sendText").value;
+        document.getElementById("sendText").value = "";
         $.getJSON("ajax/chat.php", {
             user1: getStoredValue("user1"),
             user2: getStoredValue("user2"),
