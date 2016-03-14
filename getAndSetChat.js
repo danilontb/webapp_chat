ec0b1432c6a894fb7dddbb133ca6ccd0424c71e8
getspeed <danilo89ds@gmail.com>
2016-03-14 22:34:26 +0100

    Jetzt könne es funktionieren mit der Datenübertragung.

--- getAndSetChat.js
+++ getAndSetChat.js
@@ -7,25 +7,14 @@ $(document).ready(function() {
         var text = document.getElementById("sendText").value;
         document.getElementById("sendText").value = "";
 
-        $.ajax({
-            url: "ajax/chat.php",
-            dataType: 'json',
-            data: {
-                user1: getStoredValue("user1"),
-                user2: getStoredValue("user2"),
-                text: text
-            },
-            success: callback
-        });
 
-        /* $.getJSON("ajax/chat.php", {
+
+         $.getJSON("ajax/chat.php", {
             user1: getStoredValue("user1"),
             user2: getStoredValue("user2"),
             text: text
-        }, function (data) {
-            console.log(data);
-        }).fail(function(err){
-            console.error("etwas ist fahlsch gegannen" + err);
-        })*/
+        },function(data){
+             console.log(data)
+         });
     });
 });
